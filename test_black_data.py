7d21c34039fb320236b0921690f30ab8b88dacbb
Heonwoo <162540185+heonwoos@users.noreply.github.com>
2025-11-11 00:53:24 +0900

    feat: Unicode 이미지 텐서를 출력하여 확인할 수 있는 테스트 코드 추가(test_vectorized_images.py), 훈련 셋 오류 데이터(검은 데이터) 검사 추가(test_black_data.py)

---
+++ test_black_data.py
@@ -0,0 +1,20 @@
+# 데이터셋 중 모든 데이터가 0인 데이터가 잘못 만들어짐
+
+import numpy as np
+
+for k in range(23):
+    tensor = np.load(f"obfuscation_tensors/tensor_handwriting_{k}.npy")
+    n = 0
+    first = 0
+    allow = True
+    for j, matrix in enumerate(tensor):
+        if not matrix.any():
+            n += 1
+            if allow:
+                first = j
+                allow = False
+    if n:
+        print(k, "번째 파일, ", first, "이후 총 ", n, "개의 검은 파일이 있습니다.")
+    else:
+        print(k, "번째 파일에는 검은 파일이 없습니다.")
+    print(tensor.shape)
--- test_vectorized_images.py
+++ test_vectorized_images.py
@@ -1,38 +1,135 @@
-# 'tensor_printed_1.npy' numpy 오브젝트 가져와서
-# 첫 이미지 세 개만 PIL로 띄웁니다
-
 import numpy as np
 from PIL import Image
-import random
 from one_hot_hangul import CHOSUNG_LIST, JUNGSUNG_LIST, JONGSUNG_LIST
 
-# loaded = np.load("tensor_printed_2.npy")
-# # 로그
-# print(loaded)
-# print(loaded.shape)
-# print(type(loaded))
 
-# 첫 세 개 이미지 PIL로 표시
-# n_show = min(3, loaded.shape[0])
-# random_num = random.randint(0, 10000)
-# print(random_num)
-# for i in range(n_show):
-#     arr = loaded[random_num + i]
-#     img = Image.fromarray(arr.astype(np.uint8), mode="L")
-#     img.show()
+# !!!!! 테스트할 데이터들의 PATH를 잘 입력하세요
 
-# 레이블 테스팅 코드
-loaded_label = np.load("labels_printed_0.npy")  # PATH 잘 입력 하세요
+# k = 0 # k번째 텐서와 레이블을 같이 테스팅할 수 있도록 하는 변수
+# PATH_TENSOR = f"Hangul-to-Unicode-Obfuscation-Project/unicode_tensors_{k}.npy"
+# PATH_LABEL = f"obfuscation_labels/labels_handwriting_{k}.npy"
+j = 0
+PATH_UNICODE_TENSOR = f"unicode_tensors_{j}.npy"
 
-encoding = loaded_label[0]
-for i, value in enumerate(encoding):
-    if not value:
-        continue
 
-    if i < 19:
-        print(CHOSUNG_LIST[i], end=" ")
-    elif i < 40:
-        print(JUNGSUNG_LIST[i - 19], end=" ")
+# 훈련 세트 전용 검사기 (이미지 출력)
+def test_dataset_tensor(
+    path: str, how_many: int = 3, starting_from: int = 0, reversed: bool = False
+) -> None:
+    loaded = np.load(path)
+    length = loaded.shape[0]
+
+    print("텐서 모양: ", loaded.shape)
+
+    n_show = min(how_many, length - starting_from)
+
+    if not reversed:
+        start = starting_from
+        end = starting_from + n_show - 1
     else:
-        print(JONGSUNG_LIST[i - 40])
-print(loaded_label.shape)
+        start = length - 1 - starting_from
+        end = length - starting_from - n_show
+
+    print(
+        start,
+        "번째 데이터부터 ",
+        end,
+        "번째 데이터까지 확인합니다.",
+    )
+
+    reversed_int = 1 if not reversed else -1
+
+    for i in range(start, end + reversed_int, reversed_int):
+        array = loaded[i]
+        array = array.astype(np.uint8)
+        img = Image.fromarray(array, mode="L")
+        img.show(str(i) + ".png")
+
+
+# 유니코드 이미지 전용 검사기 (이미지 출력)
+def test_unicode_tensor(
+    path: str, how_many: int = 3, starting_from: int = 0, reversed: bool = False
+) -> None:
+    loaded = np.load(path)
+    length = loaded.shape[0]
+
+    print("텐서 모양: ", loaded.shape)
+
+    n_show = min(how_many, length - starting_from)
+
+    if not reversed:
+        start = starting_from
+        end = starting_from + n_show - 1
+    else:
+        start = length - 1 - starting_from
+        end = length - starting_from - n_show
+
+    print(
+        start,
+        "번째 데이터부터 ",
+        end,
+        "번째 데이터까지 확인합니다.",
+    )
+
+    reversed_int = 1 if not reversed else -1
+
+    for i in range(start, end + reversed_int, reversed_int):
+        array = loaded[i]
+        array = np.squeeze(array)  # (64, 64, 1) -> (64, 64)로 차원 축소
+        array *= 255
+        array = array.astype(
+            np.uint8
+        )  # array를 0-255 범위의 부호 없는 8비트 정수로 변환
+        img = Image.fromarray(array, mode="L")
+        img.show(str(i) + ".png")
+
+
+# 레이블 전용 검사기 (인코딩 -> 레이블 출력)
+def test_label(
+    path: str, how_many: int = 3, starting_from: int = 0, reversed: bool = False
+) -> None:
+    loaded = np.load(path)
+    length = loaded.shape[0]
+
+    print("텐서 모양: ", loaded.shape)
+
+    n_show = min(how_many, length - starting_from)
+
+    if not reversed:
+        start = starting_from
+        end = starting_from + n_show - 1
+    else:
+        start = length - 1 - starting_from
+        end = length - starting_from - n_show
+
+    print(
+        start,
+        "번째 데이터부터 ",
+        end,
+        "번째 데이터까지 확인합니다.",
+    )
+
+    reversed_int = 1 if not reversed else -1
+
+    for i in range(start, end + reversed_int, reversed_int):
+        encoding = loaded[i]
+        for i, value in enumerate(encoding):
+            if value == 0:
+                continue
+
+            if i < 19:
+                print(CHOSUNG_LIST[i], end="")
+            elif i < 40:
+                print(JUNGSUNG_LIST[i - 19], end=" ")
+            elif i == 40:
+                print("빈칸")
+            else:
+                print(JONGSUNG_LIST[i - 40])
+
+
+# 64번 부터 순방향 5개 유니코드를 검사
+test_unicode_tensor(
+    path=PATH_UNICODE_TENSOR, how_many=5, starting_from=64, reversed=False
+)
+
+# test_label(path=PATH_LABEL, how_many=5, starting_from=20000, reversed=False)
